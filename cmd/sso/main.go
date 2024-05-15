53160a05de3ede618f86b1d7a33049dacff04278
arxonic <blasterjaxxx33@gmail.com>
2024-05-15 22:14:52 +0300

    add cors to gateway

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -3,15 +3,26 @@ package main
 import (
 	"cmd/sso/main.go/internal/app"
 	"cmd/sso/main.go/internal/config"
+	"context"
 	"log/slog"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"github.com/rs/cors"
+
+	ssov1 "github.com/arxonic/protos/gen/go/sso"
 )
 
 var (
-	tokenTTL = 1 * time.Hour
+	tokenTTL    = 1 * time.Hour
+	httpAddress = "localhost:8088"
 )
 
 func main() {
@@ -24,7 +35,14 @@ func main() {
 	application := app.New(log, cfg.Grpc.Port, cfg.StoragePath, tokenTTL)
 	// обернуть в приложение бд
 
+	ctx := context.Background()
+
 	go application.GRPCSrv.MustRun()
+	go func() {
+		if err := startHttpServer(ctx, log); err != nil {
+			os.Exit(1)
+		}
+	}()
 
 	stop := make(chan os.Signal, 1)
 	signal.Notify(stop, syscall.SIGTERM, syscall.SIGINT)
@@ -49,4 +67,30 @@ func setupLogger(env string) *slog.Logger {
 		)
 	}
 	return log
+}
+
+func startHttpServer(ctx context.Context, log *slog.Logger) error {
+	mux := runtime.NewServeMux()
+
+	withCors := cors.New(cors.Options{
+		AllowOriginFunc:  func(origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"ACCEPT", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}).Handler(mux)
+
+	opts := []grpc.DialOption{
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	}
+
+	err := ssov1.RegisterAuthHandlerFromEndpoint(ctx, mux, ":44044", opts)
+	if err != nil {
+		return err
+	}
+
+	log.Info("starting REST API running", slog.String("addr", httpAddress))
+
+	return http.ListenAndServe(httpAddress, withCors)
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module cmd/sso/main.go
 go 1.22.2
 
 require (
-	github.com/arxonic/protos v0.0.3
+	github.com/arxonic/protos v0.1.1
 	github.com/brianvoe/gofakeit/v6 v6.28.0
 	github.com/golang-jwt/jwt/v5 v5.2.1
 	github.com/golang-migrate/migrate/v4 v4.17.1
@@ -17,16 +17,22 @@ require (
 require (
 	github.com/BurntSushi/toml v1.3.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-chi/chi v1.5.5
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.19.1 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/kr/text v0.2.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rogpeppe/go-internal v1.12.0 // indirect
+	github.com/rs/cors v1.11.0
 	go.uber.org/atomic v1.7.0 // indirect
-	golang.org/x/net v0.21.0 // indirect
+	golang.org/x/net v0.24.0 // indirect
 	golang.org/x/sys v0.19.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20240227224415-6ceb2ff114de // indirect
-	google.golang.org/protobuf v1.33.0 // indirect
+	golang.org/x/text v0.15.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20240513163218-0867130af1f8 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240509183442-62759503f434 // indirect
+	google.golang.org/protobuf v1.34.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
 )
